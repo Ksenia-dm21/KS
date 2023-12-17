0071ad874f9b1d69b148e9f7e25685913913d86f
Ksenia-dm21 <144418178+Ksenia-dm21@users.noreply.github.com>
2023-12-17 16:53:59 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+$(function() {
+/* nav toggle*/
+$("#nav_toggle").on("click", function(event) {
+  event.preventDefault();
+
+  $(this).toggleClass("active");
+  $("#nav").toggleClass("active");
+});
+
+});
